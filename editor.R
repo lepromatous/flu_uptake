35bb2c7715b3807b46a796bb125f1ac933b3b76b
Timothy Wiemken <tim.wiemken@gmail.com>
2022-04-21 09:29:32 +0100

    updates

--- editor.R
+++ editor.R
@@ -6,6 +6,8 @@ library(albersusa)
 library(Hmisc)
 library(vroom)
 library(tidycensus)
+library(biscale)
+library(cowplot)
 
 # get population by county from tidycensus ----
 get_estimates(
@@ -85,12 +87,12 @@ mapme %>%
 
 # bin that rate ----
 source("~/Library/CloudStorage/OneDrive-Pfizer/Documents/Research/zz useful R code/map binning.R")
-mapme2 <- map_breaks(.data = mapme, var = "rate_area", newvar = "rate_area_cat", classes = 4, style = "headtails", clean_labels = TRUE, dig_lab = 10)
+mapme2 <- map_breaks(.data = mapme, var = "rate_area", newvar = "rate_area_cat", classes = 4, style = "quantile", clean_labels = TRUE, dig_lab = 10)
   
 
 # map test ----
 ggplot() +
-  geom_sf(data=mapme2, aes(fill=rate_area_cat)) +
+  geom_sf(data=mapme2, aes(fill=rate_area_cat), size=0.2) +
   scale_fill_manual(values=RColorBrewer::brewer.pal(n=6, "Set1"))
 
 
@@ -118,8 +120,47 @@ df_uptake$fips <- stringr::str_pad(df_uptake$fips, width=5, pad="0", side="left"
 
 # merge with map ----
 df <- merge(mapme2, df_uptake, by="fips", all.x=T)
+df$coverage_estimate <- as.numeric(df$coverage_estimate)
+
+# map test ----
+ggplot() +
+  geom_sf(data=df, aes(fill=rev(coverage_estimate)), size=0.2)
+
 
 # clean environment ----
 
 rm(list=setdiff(ls(), c("df")))
 gc()
+
+# get SVI from other project ----
+svi <- vroom::vroom("/Users/timothywiemken/Library/CloudStorage/OneDrive-Pfizer/Documents/Research/github/COVID disparities/extra data/prener.csv")
+svi %>%
+  janitor::clean_names() -> svi
+# merge and drop svi ----
+df <- merge(df, svi, by.x="fips", by.y="geoid", all.x=T)
+
+# create biscale data ----
+data <- bi_class(df, x = svi, y = coverage_estimate, style = "quantile", dim = 3)
+
+# map biscale ----
+map <- ggplot() +
+  geom_sf(data = data, mapping = aes(fill = bi_class), color = "white", size = 0.1, show.legend = FALSE) +
+  bi_scale_fill(pal = custom_pal, dim = 3) +
+  labs(
+    title = "Influenza Vaccine Uptake and SVI",
+  ) +
+  bi_theme()
+legend <- bi_legend(pal = custom_pal,
+                    dim = 3,
+                    xlab = "Higher SVI ",
+                    ylab = "Higher Uptake ",
+                    size = 8)
+finalPlot <- ggdraw() +
+  draw_plot(map, 0, 0, 1, 1) +
+  draw_plot(legend, 0.0, .65, 0.2, 0.2)
+finalPlot
+
+custom_pal <- bi_pal_manual(val_1_1 = "#d73027", val_1_2 = "#f46d43", val_1_3 = "#fdae61",
+                            val_2_1 = "#fee08b", val_2_2 = "#ffffbf", val_2_3 = "#d9ef8b",
+                            val_3_1 = "#a6d96a", val_3_2 = "#66bd63", val_3_3 = "#1a9850"
+                            )
