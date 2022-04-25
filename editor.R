766b7be634a3d566eafdbfff4ce2dff106c818e7
Timothy Wiemken <tim.wiemken@gmail.com>
2022-04-25 11:03:36 +0100

    updates

--- editor.R
+++ editor.R
@@ -91,10 +91,10 @@ mapme2 <- map_breaks(.data = mapme, var = "rate_area", newvar = "rate_area_cat",
   
 
 # map test ----
-ggplot() +
-  geom_sf(data=mapme2, aes(fill=rate_area_cat), size=0.2) +
-  scale_fill_manual(values=RColorBrewer::brewer.pal(n=6, "Set1"))
-
+# ggplot() +
+#   geom_sf(data=mapme2, aes(fill=rate_area_cat), size=0.2) +
+#   scale_fill_manual(values=RColorBrewer::brewer.pal(n=6, "Set1"))
+# 
 
 
 # uptake data ---- 
@@ -123,9 +123,9 @@ df <- merge(mapme2, df_uptake, by="fips", all.x=T)
 df$coverage_estimate <- as.numeric(df$coverage_estimate)
 
 # map test ----
-ggplot() +
-  geom_sf(data=df, aes(fill=rev(coverage_estimate)), size=0.2)
-
+# ggplot() +
+#   geom_sf(data=df, aes(fill=rev(coverage_estimate)), size=0.2)
+# 
 
 # clean environment ----
 
@@ -142,15 +142,24 @@ df <- merge(df, svi, by.x="fips", by.y="geoid", all.x=T)
 # county deprivation ----
 #devtools::install_github("redivis/redivis-r", ref="main")
 #https://redivis.com/workspace/settings/tokens
-library(redivis)
-Sys.setenv(REDIVIS_API_TOKEN = "AAABFDynqJ9I9vk1cQgIQCsjKxYZkQkn")
+# library(redivis)
+# Sys.setenv(REDIVIS_API_TOKEN = "AAABFDynqJ9I9vk1cQgIQCsjKxYZkQkn")
+# 
+# user <- redivis::user("cdpdemo")
+# dataset <- user$dataset("area_deprivation_index_adi:axrk:v1_0")
+# table <- dataset$table("adi_by_county:dbk0")
+# 
+# # Load table as tidyverse tibble
+# adi <- table$to_tibble()
+# 
+# write.table(adi[,c("county_fips_code", "year", "area_deprivation_index_percent")], "~/Desktop/adi.csv", sep=",", row.names=F, na="")
 
-user <- redivis::user("cdpdemo")
-dataset <- user$dataset("area_deprivation_index_adi:axrk:v1_0")
-table <- dataset$table("adi_by_county:dbk0")
+adi <- read.csv("/Users/timothywiemken/Library/CloudStorage/OneDrive-Pfizer/Documents/Research/github/flu_uptake/adi.csv")
 
-# Load table as tidyverse tibble
-adi <- table$to_tibble()
+adi %>%
+  mutate(
+    stringr::str_pad(county_fips_code, side="left", pad="0", width=5)
+  ) -> adi
 
 adi %>%
   group_by(county_fips_code) %>%
@@ -182,9 +191,38 @@ df$rate_sqmi <- df$`n()` / df$area_sqmi *100
 
 
 
-# create biscale data ----
-data <- bi_class(df, x = adi_diff, y = coverage_estimate, style = "quantile", dim = 3)
+### add in covid uptake
+covid.uptake <- "https://data.cdc.gov/resource/8xkx-amqh.json?$where=date='2022-04-01'"
+tokenz<-'chCxsk4zel6QXbaemotF65C9L'
 
+read.socrata(
+  covid.uptake,
+  app_token = tokenz,
+  #####
+  email     = "tim.wiemken@gmail.com",
+  password  =  "ThisIsNotAGoodP@ssw0rd!!!" 
+) -> covid.uptake
+
+covid.uptake %>%
+  mutate(
+    stringr::str_pad(fips, side="left", pad="0", width=5)
+  ) -> covid.uptake
+
+
+df <- merge(df, covid.uptake, by="fips", all.x=T)
+df$series_complete_12pluspop_pct <- as.numeric(df$series_complete_12pluspop_pct)
+
+# ltSD <- sd(df$adi_diff[df$adi_diff <0], na.rm=T)
+# gtSD <- sd(df$adi_diff[df$adi_diff >0], na.rm=T)
+# 
+# df$adi_sd <- 2 ## middle - no change
+# df$adi_sd[df$adi_diff <0 & df$adi_diff < (-ltSD)] <-  1 ## adi improved (decreased = good)
+# df$adi_sd[df$adi_diff >0 & df$adi_diff > gtSD] <-  3 ## adi got worse
+
+
+ # create biscale data ----
+data <- bi_class(df, x = series_complete_12pluspop_pct, y = coverage_estimate, style = "quantile", dim = 3)
+# 
 # colors ----
 custom_pal <- bi_pal_manual(val_1_1 = "#d73027", val_1_2 = "#f46d43", val_1_3 = "#fdae61",
                             val_2_1 = "#fee08b", val_2_2 = "#ffffbf", val_2_3 = "#d9ef8b",
@@ -196,29 +234,31 @@ map <- ggplot() +
   geom_sf(data = data, mapping = aes(fill = bi_class), color = "white", size = 0.1, show.legend = FALSE) +
   bi_scale_fill(pal = "DkBlue", dim = 3) +
   labs(
-    title = "Influenza Vaccine Uptake and 2018/2020 ADI Change",
+    title = "Influenza Vaccine Uptake COVID-19 Vaccine uptake",
   ) +
   bi_theme()
 legend <- bi_legend(pal = "DkBlue",
                     dim = 3,
-                    xlab = "Increase in ADI",
-                    ylab = "Higher Uptake ",
+                    xlab = "Higher \nCOVID \nUptake (12+) ",
+                    ylab = "Higher \nInfluenza \nUptake (6m+) ",
                     size = 8)
 finalPlot <- ggdraw() +
   draw_plot(map, 0, 0, 1, 1) +
   draw_plot(legend, 0.0, .65, 0.2, 0.2)
 finalPlot
 
-
-source("~/Library/CloudStorage/OneDrive-Pfizer/Documents/Research/zz useful R code/map binning.R")
-df <- map_breaks(.data = df, var = "adi_diff", newvar = "adi_diff_cat", classes = 3, style = "quantile", clean_labels = TRUE, dig_lab = 10)
-
-
-ggplot() +
-  geom_sf(data = df, mapping = aes(fill = adi_diff_cat), color = "white", size = 0.1) +
-  scale_fill_manual(values = rev(RColorBrewer::brewer.pal(3, "Set1")), labels = c("Got Better", "Stayed the Same", "Got Worse", "Missing"), na.value="gray65") +
-  labs(
-    fill = "2018/2020 Delta ADI"
-  ) + 
-  theme_map()
-
+# 
+# source("~/Library/CloudStorage/OneDrive-Pfizer/Documents/Research/zz useful R code/map binning.R")
+# df <- map_breaks(.data = df, var = "adi_diff", newvar = "adi_diff_cat", classes = 3, style = "quantile", clean_labels = TRUE, dig_lab = 10)
+# 
+# 
+# 
+#              
+# ggplot() +
+#   geom_sf(data = df, mapping = aes(fill = adi_sd), color = "white", size = 0.1) +
+#   scale_fill_manual(values = rev(RColorBrewer::brewer.pal(3, "Set1")), labels = c("Got Better", "Stayed the Same", "Got Worse", "Missing"), na.value="gray65") +
+#   labs(
+#     fill = "2018/2020 Delta ADI"
+#   ) + 
+#   theme_map()
+# 
